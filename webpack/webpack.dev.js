aa89536a09108814539a3f5cfe7853c10630eefe
chris <chrisivey01@gmail.com>
2020-02-10 16:55:10 -0600

    i am so frustrated

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,34 @@
+var webpackMerge = require('webpack-merge');
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'cheap-module-eval-source-map',
+  output: {
+    path: helpers.root('dist'),
+    publicPath: 'http://localhost:8080/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: { configFileName: helpers.root('src', 'client', 'tsconfig.json') }
+          } , 'angular2-template-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // new ExtractTextPlugin('[name].css')
+  ],
+  devServer: {
+    historyApiFallback: true,
+    stats: 'minimal'
+  }
+});
